58712931925b800e04cf0bb52d9b162ec8fe0361
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-06-18 17:47:26 +0200

    Initial commit

---
+++ src/test/java/de/bouteille93/file_uploader/FileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bouteille93.file_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
