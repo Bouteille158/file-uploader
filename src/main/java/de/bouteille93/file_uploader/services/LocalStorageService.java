48e6cdbf2ef17496efbf2992db7c8c1163e7b681
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-06-19 16:25:27 +0200

    feat: save file to local storage disk
    todo: save info in db

--- src/main/java/de/bouteille93/file_uploader/services/LocalStorageService.java
+++ src/main/java/de/bouteille93/file_uploader/services/LocalStorageService.java
@@ -1,5 +1,10 @@
 package de.bouteille93.file_uploader.services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import de.bouteille93.file_uploader.interfaces.StorageInterface;
 import de.bouteille93.file_uploader.models.FileData;
 
@@ -7,15 +12,36 @@ public class LocalStorageService implements StorageInterface {
     @Override
     public String upload(FileData file) {
 
-        // save file to local storage
-        System.out.println("File saved to local storage");
+        // TODO Generate download link
+        file.getFileInfo().setUrl("uploads/" + file.getFileInfo().getId());
 
-        return file.getFileInfo().getUrl();
+        // TODO Save file info in database
+
+        try {
+            // Définir le chemin où le fichier sera enregistré
+            Path path = Paths.get("uploads/" + file.getFileInfo().getId());
+
+            // Vérifier si le répertoire existe
+            if (!Files.exists(path.getParent())) {
+                // Si le répertoire n'existe pas, le créer
+                Files.createDirectories(path.getParent());
+            }
+
+            // Écrire le fichier sur le disque
+            Files.write(path, file.getData());
+
+            return file.getFileInfo().getUrl();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
 
     }
 
     @Override
     public FileData download(String attachment_id) {
+        // TODO Generate download link
+
         return null;
     }
 }
