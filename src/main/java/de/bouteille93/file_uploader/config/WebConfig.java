24b650298c1dce9d3c95784a4b0a994683cffcff
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-07-11 18:11:11 +0200

    feat: add CORS
    allow all

---
+++ src/main/java/de/bouteille93/file_uploader/config/WebConfig.java
@@ -0,0 +1,20 @@
+package de.bouteille93.file_uploader.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // TODO update CORS, currently allow all
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
